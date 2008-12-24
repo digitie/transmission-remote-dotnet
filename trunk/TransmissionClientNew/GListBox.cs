b0b8067e3d996afc4ae56bbc983e840b52eb2356
alan@eth0.org.uk <alan@eth0.org.uk@bb819956-c407-11dd-8bad-cde060d8fd3c>
2008-12-24 01:08:11 +0000

    Stop GListBox flickering

--- trunk/TransmissionClientNew/GListBox.cs
+++ trunk/TransmissionClientNew/GListBox.cs
@@ -18,8 +18,47 @@ namespace TransmissionRemoteDotnet
         public GListBox()
         {
             this.DrawMode = DrawMode.OwnerDrawFixed;
+            this.SetStyle(  
+                ControlStyles.OptimizedDoubleBuffer |  
+                ControlStyles.ResizeRedraw |  
+                ControlStyles.UserPaint,  
+                true);  
         }
-        
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Region iRegion = new Region(e.ClipRectangle);
+            e.Graphics.FillRegion(new SolidBrush(this.BackColor), iRegion);
+            if (this.Items.Count > 0)
+            {
+                for (int i = 0; i < this.Items.Count; ++i)
+                {
+                    System.Drawing.Rectangle irect = this.GetItemRectangle(i);
+                    if (e.ClipRectangle.IntersectsWith(irect))
+                    {
+                        if ((this.SelectionMode == SelectionMode.One && this.SelectedIndex == i)
+                       || (this.SelectionMode == SelectionMode.MultiSimple && this.SelectedIndices.Contains(i))
+                        || (this.SelectionMode == SelectionMode.MultiExtended && this.SelectedIndices.Contains(i)))
+                        {
+                            OnDrawItem(new DrawItemEventArgs(e.Graphics, this.Font,
+                                irect, i,
+                                DrawItemState.Selected, this.ForeColor,
+                                this.BackColor));
+                        }
+                        else
+                        {
+                            OnDrawItem(new DrawItemEventArgs(e.Graphics, this.Font,
+                                irect, i,
+                                DrawItemState.Default, this.ForeColor,
+                                this.BackColor));
+                        }
+                        iRegion.Complement(irect);
+                    }
+                }
+            }
+            base.OnPaint(e);
+        }
+
         protected override void OnDrawItem(System.Windows.Forms.DrawItemEventArgs e)
         {
             e.DrawBackground();
