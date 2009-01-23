f34e73651600c0895b278fb5497eaac9a4d00226
alan <alan@eth0.org.uk>
2009-01-23 13:24:13 +0000

    Sorting for errorsListView

---
+++ TransmissionClientNew/ErrorsListViewColumnSorter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TransmissionRemoteDotnet.Comparers;
+
+namespace TransmissionRemoteDotnet
+{
+    public class ErrorsListViewColumnSorter : IComparer
+    {
+        private int columnToSort;
+        private SortOrder orderOfSort;
+        private IComparer objectCompare;
+
+        public ErrorsListViewColumnSorter()
+        {
+            columnToSort = 0;
+            orderOfSort = SortOrder.Descending;
+            objectCompare = new ListViewTextComparer(0, false);
+        }
+
+        public int Compare(object x, object y)
+        {
+            int compareResult;
+            compareResult = objectCompare.Compare(x, y);
+            if (orderOfSort == SortOrder.Ascending)
+            {
+                return compareResult;
+            }
+            else if (orderOfSort == SortOrder.Descending)
+            {
+                return (-compareResult);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public SortOrder Order
+        {
+            set
+            {
+                orderOfSort = value;
+            }
+            get
+            {
+                return orderOfSort;
+            }
+        }
+
+        public int SortColumn
+        {
+            set
+            {
+                columnToSort = value;
+                switch (columnToSort)
+                {
+                    default:
+                        objectCompare = new ListViewTextComparer(columnToSort, false);
+                        break;
+                }
+            }
+            get
+            {
+                return columnToSort;
+            }
+        }
+    }
+}
