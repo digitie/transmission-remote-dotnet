761d6ce6a8767553b3e02343345c77daf67d6dec
alan <alan@eth0.org.uk>
2009-03-14 01:23:54 +0000

    Maybe many will think this is either bloat or confusing, but I find it very useful so this a 'hidden feature'. If you set the registry keys _plinkPath and _plinkCmd you will see a box (since I'm using it to extract downloaded archives) icon appear. When you click it, plink (command line PuTTy) will connect to a session with the same hostname and run _plinkCmd with the torrent directory as an argument.

---
+++ TransmissionClientNew/PlinkCmd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace TransmissionRemoteDotnet
+{
+    class PlinkCmd
+    {
+        private Torrent t;
+
+        public PlinkCmd(Torrent t)
+        {
+            this.t = t;
+        }
+
+        public void Start()
+        {
+            Process.Start(LocalSettingsSingleton.Instance.PlinkPath, String.Format("\"{0}\" \"{1}\" \"{2}\"", LocalSettingsSingleton.Instance.Host, LocalSettingsSingleton.Instance.PlinkCmd, String.Format("{0}/{1}", this.t.DownloadDir, this.t.Name)));
+        }
+    }
+}
