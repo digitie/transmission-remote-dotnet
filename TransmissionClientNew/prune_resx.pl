6f94c3881795617c370cc5179b48384af4d31cfd
alan <alan@eth0.org.uk>
2009-04-07 20:25:31 +0000

    The script missed a few things

--- TransmissionClientNew/prune_resx.pl
+++ TransmissionClientNew/prune_resx.pl
@@ -3,7 +3,23 @@ use strict;
 
 my $skip = 0;
 my $output = "";
-open(RESX, "<$ARGV[0]");
+
+if (@ARGV < 1)
+{
+	print STDERR "usage: ./prune_resx.pl <directory>";
+	exit 1;
+}
+
+opendir(DIR, $ARGV[0]);
+my @files = readdir(DIR);
+closedir(DIR);
+
+foreach my $file (@files)
+{
+if ($file =~ m/\.[a-z][a-z]-[A-Z][A-Z]\.resx/i)
+{
+$output = "";
+open(RESX, "<$file");
 while(<RESX>)
 {
 	if ($skip > 0)
@@ -16,17 +32,18 @@ while(<RESX>)
 	}
 	if (m/\<data name=\"(.*?)\"/)
 	{
-		if ($1 !~ m/\.(Text|Item\d+)$/)
+		if ($1 !~ m/\.(Text|Items\d+)$/)
 		{
 			$skip = 1;
 			next;
 		}
 	}
 	$output .= $_;
-	#print $_;
 }
 close(RESX);
 
-open(RESX, ">$ARGV[0]");
+open(RESX, ">$file");
 print RESX $output;
 close(RESX);
+}
+}
