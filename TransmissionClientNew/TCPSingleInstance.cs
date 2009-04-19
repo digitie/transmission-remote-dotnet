c17bbae30797a405d8b4ff5f832d1476a7deb0ec
alan <alan@eth0.org.uk>
2009-04-19 11:54:19 +0000

    Use Windows style newlines in the GPL headers

--- TransmissionClientNew/TCPSingleInstance.cs
+++ TransmissionClientNew/TCPSingleInstance.cs
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#if !DOTNET35
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -141,3 +142,4 @@ namespace TransmissionRemoteDotnet
         #endregion
     }
 }
+#endif
