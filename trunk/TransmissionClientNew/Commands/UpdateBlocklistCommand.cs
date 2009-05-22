dd17286a54021d175ec4d45956bb47493b1f86b8
alan@eth0.org.uk <alan@eth0.org.uk@bb819956-c407-11dd-8bad-cde060d8fd3c>
2009-05-22 13:02:32 +0000

    Restore .NET 2.0 compatability

--- trunk/TransmissionClientNew/Commands/UpdateBlocklistCommand.cs
+++ trunk/TransmissionClientNew/Commands/UpdateBlocklistCommand.cs
@@ -17,7 +17,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Jayrock.Json;
 using System.Windows.Forms;
--- trunk/TransmissionClientNew/Comparers/ListViewDoneDateComparer.cs
+++ trunk/TransmissionClientNew/Comparers/ListViewDoneDateComparer.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
